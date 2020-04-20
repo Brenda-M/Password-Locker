edca65c171a297488b3bce437a5ab82e1080d48e
Brenda-M <brendanjuguna1@gmail.com>
2020-04-20 10:59:04 +0300

    add test case to ensure objects are being instantiated properly

--- users.py
+++ users.py
@@ -1,6 +1,13 @@
-class Users:
+class User:
   """
-  Class that generates new instances of contacts
+  Class that generates new instances of users
 
   """
-  def __init__(self, first_name, last_name, password)
+  def __init__ (self, user_name, account_name, password):
+    self.user_name = user_name
+    self.account_name = account_name
+    self.password = password
+  
+  
+
+
--- users_test.py
+++ users_test.py
@@ -1,1 +1,21 @@
 import unittest
+from users import User
+
+class TestUsers(unittest.TestCase):
+  """
+  This is a test class that defines the test cases for the users class behavior
+
+  """
+  def setUp(self):
+    '''
+    set up method to run before each test case to ensure objects are being instantiated correctly
+    '''
+    self.new_user = User("Brenda", "Facebook", "4676jl")
+  
+  def test_init(self):
+    self.assertEqual(self.new_user.user_name, "Brenda")
+    self.assertEqual(self.new_user.account_name, "Facebook")
+    self.assertEqual(self.new_user.password,"4676jl")
+  
+  if __name__ == '__main__':
+    unittest.main()
