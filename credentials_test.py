d238761209a0e0aaf49af44e41c9dc25e758e466
Brenda-M <brendanjuguna1@gmail.com>
2020-04-20 14:59:53 +0300

    add test case for saving credentials

---
+++ credentials_test.py
@@ -0,0 +1,27 @@
+import unittest
+from user_credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+  def setUp(self):
+    self.new_credential = Credentials("Bree95", "Facebook", "password1")
+
+  def tearDown(self):
+    Credentials.credentials_list = []
+
+  def test_init(self):
+    self.assertEqual(self.new_credential.user_name, "Bree95")
+    self.assertEqual(self.new_credential.account_name, "Facebook")
+    self.assertEqual(self.new_credential.password, "password1")
+
+  def test_save_credentials(self):
+    self.new_credential.save_credentials()
+    self.assertEqual(len(Credentials.credentials_list), 1)
+
+ 
+  
+  
+
+
+if __name__ == '__main__':
+  unittest.main()
