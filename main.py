90ae54fa7c1145a6bb0a0d3e5a2bb5f9fba00ca5
Brenda-M <brendanjuguna1@gmail.com>
2020-04-21 01:37:11 +0300

    use if and elif statments to display and copy contacts

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def verify_user(user_name, password):
   finding_user = Credentials.find_user(user_name, password)
   return finding_user
 
+def create_credential(user_name, account_name, password):
+  new_credential = Credentials(user_name, account_name, password)
+  return new_credential
+
 def save_credentials(credential):
   Credentials.save_credentials(credential)
 
@@ -25,6 +29,10 @@ def display_credential(user_name):
 
 def copy_credential(account_name):
   return Credentials.copy_credentials(account_name)
+
+def generate_password():
+  gen_password = Credentials.generate_password()
+  return gen_password
 
 def main(): 
 
@@ -36,24 +44,20 @@ def main():
 
     if short_code == 'ca':
       print("To create a new account: ")
-      first_name = input ("Enter your first name: ")
-      last_name = input ("Enter your last name: ")
-      nick_name = input ("Enter a nickname: ")
-      password = input (" Enter your password: ")
-
-      save_users(create_user(first_name, last_name, nick_name, password))
+      user_name = input ("Enter a username: ")
+      password = input (" Create a password: ")
+      save_user(create_user(user_name, password))
       print('\n')
-      print(f"Thank you for joining us {first_name} {last_name}")
-      print(f"Your username and password are {nick_name} and {password} respectively")
-    
+      print(f"Thank you for joining us {user_name}")
+      print(f"The password to your account is {password}") 
     elif short_code == 'li':
       print("We are glad that you are here. \n Enter your details below to log in to your account")
-      nick_name = input("Nickname: ")
+      user_name = input("Username: ")
       password = str(input("Password: "))
+      user_exists = verify_user(user_name, password)
       
-      if user_exists == nick_name:
-        print(f"Welcome {nick_name}. \n Choose one of the options below to proceed")
-
+      if user_exists == user_name:
+        print(f"Welcome {user_name}. \n Choose one of the options below to proceed")
         while True:
           print("cc - Add a credential \n dc- Display credentials \n c - Copy password \n ex - Exit")    
           short_code = input ("Enter code: ").lower()
@@ -61,28 +65,37 @@ def main():
             print("Enter your credentials below")
             account_name = input("Name of site: ")
             user_name = input ("Username: ")
-
             while True:
-              print("Enter op to create your own custom password and gp to generate one")
-              password_choice == input().lower()
-
+              print("Enter: \n op to create your own custom password, \n gp to generate one \n ex to exit")
+              password_choice = input().lower()
               if password_choice == "op":
                 password = input("Enter a password: ")
                 break
               elif password_choice == "gp":
                 password = generate_password()
                 break
+              elif password_choice == "ex":
+                break
               else:
                 print("The code you have entered does not exist. Please try again")
-      
+            save_credentials(create_credential(user_name, account_name, password))
+            print(f"Your credentials have been saved succesfully. Account Name: {account_name} UserName: {user_name}. Password: {password}")
+          elif short_code == 'dc':
+            if display_credential(user_name):
+              print("Here is a list of all your credentials")
+              for credential in display_credential(user_name):
+                print(f'Account Name: {credential.account_name} - UserName: {credential.user_name} - Password: {credential.password}')
+            else:
+              print("You have not any saved credentials yet")
+          elif short_code == "c":
+            choose_site = input("Enter name of site you want to copy")
+            copy_credential(choose_site)
           elif short_code == "ex":
             print("You have been logged out. Goodbye!!")
           else:
             print("Please check your short code and try again")
-
     elif short_code == "ex":
       print("We are sorry to see you leave. Goodbye!!")
       break
-
     else:
       print("Please check your short code and try again")
