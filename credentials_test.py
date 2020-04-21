b5de245e7fc1e74cabbc551e1f9cc4c01b45e03e
Brenda-M <brendanjuguna1@gmail.com>
2020-04-21 14:19:59 +0300

    rewrite test case for copy paste

--- credentials_test.py
+++ credentials_test.py
@@ -1,13 +1,15 @@
 import unittest
+import pyperclip
 from user_credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
 
   def setUp(self):
-    self.new_credential = Credentials("Bree95", "Facebook", "password1")
+    self.new_credential = Credentials("Bree95", "Facebook", "4676jl")
 
   def tearDown(self):
     Credentials.credentials_list = []
+
 
   def test_init(self):
     '''
@@ -15,7 +17,7 @@ class TestCredentials(unittest.TestCase):
     '''
     self.assertEqual(self.new_credential.user_name, "Bree95")
     self.assertEqual(self.new_credential.account_name, "Facebook")
-    self.assertEqual(self.new_credential.password, "password1")
+    self.assertEqual(self.new_credential.password, "4676jl")
 
   def test_save_credentials(self):
     '''
@@ -55,11 +57,29 @@ class TestCredentials(unittest.TestCase):
     self.new_credential.save_credentials()
     test_credential = Credentials('Brenda', 'gmail', '4676jl' )
     test_credential.save_credentials()
-    found_credential = Credentials.find_by_user_name('Brenda')
-    self.assertEqual(found_credential.user_name, test_credential.user_name)
-  
+    test_credential2 = Credentials('Muthoni', 'Facebook','pass@word' )
+    test_credential2.save_credentials()
+    self.assertEqual(len(Credentials.find_by_user_name(test_credential.user_name)),1)
 
+  def test_copy_credential(self):
+    '''
+    Test to check if user can copy the right credentials
+    '''
+    self.new_credential.save_credentials()
+    test_credential = Credentials('Brenda', 'gmail', '4676jl' )
+    test_credential.save_credentials()
+    find_credential = None
+    for credential in Credentials.user_credentials_list:
+        find_credential = Credentials.find_by_account_name(credential.account_name)
+        return pyperclip.copy(find_credential.password)
+    Credentials.copy_credentials(self.new_credential.account_name)
+    self.assertEqual('4676jl',pyperclip.paste())
+    print(pyperclip.paste())
   
+  dr
+    
+
+
 
 
 if __name__ == '__main__':
