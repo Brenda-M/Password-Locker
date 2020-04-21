0faea306b79582769c3e3a3565bfde3209dad75a
Brenda-M <brendanjuguna1@gmail.com>
2020-04-21 13:36:27 +0300

    add docstrings to methods

--- credentials_test.py
+++ credentials_test.py
@@ -10,26 +10,33 @@ class TestCredentials(unittest.TestCase):
     Credentials.credentials_list = []
 
   def test_init(self):
+    '''
+    Test case to test if the object is initialized properly
+    '''
     self.assertEqual(self.new_credential.user_name, "Bree95")
     self.assertEqual(self.new_credential.account_name, "Facebook")
     self.assertEqual(self.new_credential.password, "password1")
 
   def test_save_credentials(self):
+    '''
+    Test case to test if the credentials object is saved into the credentials list
+
+    '''
     self.new_credential.save_credentials()
     self.assertEqual(len(Credentials.credentials_list), 1)
 
   def test_delete_credentials(self):
+    '''
+    Test case to test if user can remove credential from credentials list.
+    
+    '''
     self.new_credential.save_credentials()
     test_credential = Credentials("Brenda", "gmail", "4676jl")
     test_credential.save_credentials()
     self.new_credential.delete_credential()
     self.assertEqual(len(Credentials.credentials_list), 1)
   
-  # def test_generate_password(self):
-  #   self.test_credential = Credentials("Bree95", "gmail", "")
-  #   self.test_credential.password = generate_password()
-  #   self.assertEqual()
-  
+
   
 
 
